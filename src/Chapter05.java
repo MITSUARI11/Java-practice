13c2f9f58cd38928483f538300c821a7766d5699
GAISHI <141330448+MITSUARI11@users.noreply.github.com>
2024-07-09 22:29:55 +0900

    Chapter.05課題

---
+++ src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		
+		String word = "SG"; //string型の変数定義
+		System.out.println(word);
+		
+		word = "imagine"; //変数の再定義
+		System.out.println(word);
+		
+		final int lucky_number = 777; //定数の定義
+		System.out.println(lucky_number);
+		
+		// TODO 自動生成されたメソッド・スタブ
+		
+
+	}
+
+}
---
+++ src/Variable02.java
@@ -0,0 +1,18 @@
+
+public class Variable02 {
+	
+	static final int MIN_NUMBER = 10;
+
+	public static void main(String[] args) {
+		System.out.println(MIN_NUMBER);
+		
+		
+		final int maxValue;
+		maxValue = 100;
+		System.out.println(maxValue);
+		
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
